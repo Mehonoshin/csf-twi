bfa90a17f4dbeb5683ccf2bfe8c1cf25d73e3116
Stanislav Mekhonoshin <ejabberd@gmail.com>
2012-05-19 11:29:29 +0400

    Fix deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -37,13 +37,13 @@ set :unicorn_pid, "#{deploy_to}/shared/pids/unicorn.pid"
 # Unicorn control tasks
 namespace :deploy do
   task :restart do
-    run "if [ -f #{unicorn_pid} ]; then kill -USR2 `cat #{unicorn_pid}`; else cd #{deploy_to}/current && bundle exec unicorn -c #{unicorn_conf} -E #{rails_env} -D; fi"
+    run "if [ -f #{unicorn_pid} ] && [ -e /proc/$(cat #{unicorn_pid}) ]; then kill -USR2 `cat #{unicorn_pid}`; else cd #{deploy_to}/current && bundle exec unicorn -Dc #{unicorn_conf} -E #{rails_env}; fi"
   end
   task :start do
     run "cd #{deploy_to}/current && bundle exec unicorn -c #{unicorn_conf} -E #{rails_env} -D"
   end
   task :stop do
-    run "if [ -f #{unicorn_pid} ]; then kill -QUIT `cat #{unicorn_pid}`; fi"
+    run "[ -f #{unicorn_pid} ] && kill -QUIT `cat #{unicorn_pid}`"
   end
 end
 
